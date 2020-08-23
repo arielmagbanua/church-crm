99d30b850a56dd5756834483541a5e4257253e27
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-23 22:24:12 +0800

    Implemented navigation service for toggling the side nav.

--- src/app/components/layout/toolbar/toolbar.component.ts
+++ src/app/components/layout/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationService } from '../../../navigation.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
-
-  constructor() { }
+  constructor(private navService: NavigationService) { }
 
   ngOnInit(): void {
   }
 
+  toggleDrawer(): void {
+    this.navService.toggleDrawer();
+  }
 }
