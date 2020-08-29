cd147fdc8e956c396ac920b333693e4d5443ceda
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-30 00:31:52 +0800

    Added showing of snack bar after successfully addition of member.

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MemberDialogComponent } from './member-dialog/member-dialog.component';
+import { Member } from '../shared/member';
+import { NotifierService } from '../shared/notifier.service';
 
 @Component({
   selector: 'app-members',
@@ -8,7 +10,7 @@ import { MemberDialogComponent } from './member-dialog/member-dialog.component';
   styleUrls: ['./members.component.scss']
 })
 export class MembersComponent implements OnInit {
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private notifierService: NotifierService) { }
 
   ngOnInit(): void {
   }
@@ -16,8 +18,13 @@ export class MembersComponent implements OnInit {
   openAddMemberDialog(): void {
     const dialogRef = this.dialog.open(MemberDialogComponent);
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result: Member) => {
       console.log(`Dialog result: ${result}`);
+
+      if (result) {
+        // show snackbar to signify success
+        this.notifierService.showSimpleSnackBar('Member was added successfully.');
+      }
     });
   }
 }
