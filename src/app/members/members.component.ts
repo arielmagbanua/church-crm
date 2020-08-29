9a4d074a1c3a1c24caa0e425ea83539e77c7c9c1
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-29 20:42:13 +0800

    Added opening of dialog.

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MemberDialogComponent } from './member-dialog/member-dialog.component';
 
 @Component({
   selector: 'app-members',
@@ -6,13 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./members.component.scss']
 })
 export class MembersComponent implements OnInit {
-
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
   openAddMemberDialog(): void {
+    const dialogRef = this.dialog.open(MemberDialogComponent);
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 }
