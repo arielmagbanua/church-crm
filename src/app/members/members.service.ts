7d746bf90b264ee71afa1069c2cf12b4b504481e
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-31 22:58:08 +0800

    Added members observable to retrieve members from firestore.

--- src/app/members/members.service.ts
+++ src/app/members/members.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Member } from '../shared/member';
-import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
+import {AngularFirestore, AngularFirestoreCollection, DocumentReference} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
+  private membersCollection: AngularFirestoreCollection<Member>;
+  membersObservable: Observable<Member[]>;
 
-  constructor(private firestore: AngularFirestore) { }
+  constructor(private firestore: AngularFirestore) {
+    this.membersCollection = firestore.collection<Member>('members');
+    this.membersObservable = this.membersCollection.valueChanges();
+  }
+
+  get members(): Observable<Member[]>{
+    return this.membersObservable;
+  }
 
   addMember(member: Member): Promise<DocumentReference> {
-    return this.firestore.collection<Member>('members')
-      .add(member);
+    return this.membersCollection.add(member);
   }
 
   updateMember(id: string, member: Member): void {
