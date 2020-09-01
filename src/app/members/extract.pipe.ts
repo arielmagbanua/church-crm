57a770100c744c6d86263db9ffa74e40b3d1edd8
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-09-01 22:42:32 +0800

    Added new cases.

--- src/app/members/extract.pipe.ts
+++ src/app/members/extract.pipe.ts
@@ -15,6 +15,21 @@ export class ExtractPipe implements PipeTransform {
       case 'gender':
         value = member.gender === 'M' ? 'male' : 'female';
         break;
+      case 'membershipDate':
+        value = member.membershipDate ? member.membershipDate.toDate().toLocaleDateString() : 'N/A';
+        break;
+      case 'birthdate':
+        value = member.birthdate.toDate().toLocaleDateString();
+        break;
+      case 'smallGroup':
+        value = member.smallGroup ?? 'N/A';
+        break;
+      case 'address':
+        value = member.address ?? 'N/A';
+        break;
+      case 'photo':
+        value = member.photo ?? 'https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png';
+        break;
       default:
         value = '';
     }
