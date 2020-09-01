9b3dafc687dd4b4f348ba6d467eba24607013a46
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-09-01 21:26:26 +0800

    Added uploading of photo to firebase cloud storage.

--- src/app/members/members.service.ts
+++ src/app/members/members.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Member } from '../shared/member';
-import {AngularFirestore, AngularFirestoreCollection, DocumentReference} from '@angular/fire/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  DocumentReference
+} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +15,26 @@ export class MembersService {
   private membersCollection: AngularFirestoreCollection<Member>;
   membersObservable: Observable<Member[]>;
 
-  constructor(private firestore: AngularFirestore) {
+  constructor(
+    private firestore: AngularFirestore,
+    private fireStorage: AngularFireStorage
+  ) {
     this.membersCollection = firestore.collection<Member>('members');
     this.membersObservable = this.membersCollection.valueChanges();
   }
 
-  get members(): Observable<Member[]>{
+  get members(): Observable<Member[]> {
     return this.membersObservable;
   }
 
   addMember(member: Member): Promise<DocumentReference> {
     return this.membersCollection.add(member);
+  }
+
+  uploadPhoto(file: File): AngularFireUploadTask {
+    const filePath = new Date().valueOf() + '-' + file.name;
+    const fileRef = this.fireStorage.ref(filePath);
+    return this.fireStorage.upload(filePath, file);
   }
 
   updateMember(id: string, member: Member): void {
