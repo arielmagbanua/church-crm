23d4f7cfc13c629c6be32d07b92e73f8820e711d
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-09-01 23:55:53 +0800

    Optimized the app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,47 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MemberListComponent } from './members/member-list/member-list.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { CdkTableModule } from '@angular/cdk/table';
-import { CdkTreeModule } from '@angular/cdk/tree';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MembersComponent } from './members/members.component';
 import { SmallGroupsComponent } from './small-groups/small-groups.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MemberDialogComponent } from './members/member-dialog/member-dialog.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { ExtractPipe } from './members/extract.pipe';
-import { MaterialFileInputModule } from 'ngx-material-file-input';
 import { AngularFireStorageModule, BUCKET } from '@angular/fire/storage';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MembersComponent,
     SmallGroupsComponent,
     DashboardComponent,
-    MemberDialogComponent,
-    MemberListComponent,
-    ExtractPipe
   ],
   imports: [
     BrowserModule,
@@ -51,26 +30,13 @@ import { AngularFireStorageModule, BUCKET } from '@angular/fire/storage';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    CdkTableModule,
-    CdkTreeModule,
-    MatButtonModule,
-    MatDatepickerModule,
-    MatDialogModule,
     MatDividerModule,
-    MatInputModule,
     MatListModule,
-    MatNativeDateModule,
-    MatPaginatorModule,
-    MatSelectModule,
     MatSidenavModule,
-    MatSnackBarModule,
-    MatSortModule,
-    MatTableModule,
-    ReactiveFormsModule,
+    SharedModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireStorageModule,
-    MaterialFileInputModule
+    AngularFireStorageModule
   ],
   providers: [
     { provide: BUCKET, useValue: 'gpdavao.appspot.com' }
