961fea828a74c136ede2bc36df4d59a9c1cab037
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-09-04 22:03:08 +0800

    Imported needed modules and added route config.

--- src/app/small-groups/small-groups.module.ts
+++ src/app/small-groups/small-groups.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SmallGroupsComponent } from './small-groups.component';
+import { SharedModule } from '../shared/shared.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SmallGroupsComponent
+  }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SmallGroupsComponent
+  ],
   imports: [
-    CommonModule
+    SharedModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class SmallGroupsModule { }
