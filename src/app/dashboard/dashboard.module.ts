0e3c7fb1b1694ada785ab96114a0941ab6b6486e
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-09-04 22:10:14 +0800

    Added the needed components and module.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+import { SharedModule } from '../shared/shared.module';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent
+  ],
   imports: [
-    CommonModule
+    SharedModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class DashboardModule { }
