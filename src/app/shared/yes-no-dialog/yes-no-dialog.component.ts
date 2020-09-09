9dfeb221c174ed09a90d0b075e72800348a6d632
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-09-09 22:46:45 +0800

    Added data and method for the dialog.

--- src/app/shared/yes-no-dialog/yes-no-dialog.component.ts
+++ src/app/shared/yes-no-dialog/yes-no-dialog.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+export interface ConfirmData {
+  title: string;
+  message: string;
+  positiveText: string;
+  negativeText: string;
+  result?: any;
+}
 
 @Component({
   selector: 'app-yes-no-dialog',
@@ -6,10 +15,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./yes-no-dialog.component.scss']
 })
 export class YesNoDialogComponent implements OnInit {
+  /**
+   * Dialog title
+   */
+  title = '';
 
-  constructor() { }
+  /**
+   * Dialog message
+   */
+  message = '';
+
+  /**
+   * Positive button text
+   */
+  negativeText = 'Yes';
+
+  /**
+   * Negative button text
+   */
+  positiveText = 'To';
+
+  constructor(
+    public dialogRef: MatDialogRef<YesNoDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ConfirmData) {}
 
   ngOnInit(): void {
+    this.title = this.data.title;
+    this.message = this.data.message;
+    this.positiveText = this.data.positiveText;
+    this.negativeText = this.data.negativeText;
   }
 
+  /**
+   * Affirmation click method.
+   */
+  affirm(): void {
+    this.dialogRef.close(this.data.result);
+  }
 }
