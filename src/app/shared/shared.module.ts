4b3fcbad05c0085b234e373eb421b03f7c3b5b42
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-09-09 22:48:46 +0800

    Added Mat dialog ang yes no dialog component.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,15 +4,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { YesNoDialogComponent } from './yes-no-dialog/yes-no-dialog.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [YesNoDialogComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     MatSnackBarModule,
     MatIconModule,
     MatButtonModule,
+    MatDialogModule
   ],
   exports: [
     CommonModule,
