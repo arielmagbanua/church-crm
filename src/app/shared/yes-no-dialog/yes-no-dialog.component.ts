1d2d11a33d0433da4de1a3806a7c044ed4cb03be
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-09-09 22:08:34 +0800

    Added yes no dialog component.

---
+++ src/app/shared/yes-no-dialog/yes-no-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yes-no-dialog',
+  templateUrl: './yes-no-dialog.component.html',
+  styleUrls: ['./yes-no-dialog.component.scss']
+})
+export class YesNoDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
