b6a147ae1df7631a40d4f3e06a6978d261f88392
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-09-20 01:27:39 +0800

    Code clean up and added doc blocks.

--- src/app/members/members.service.ts
+++ src/app/members/members.service.ts
@@ -23,14 +23,27 @@ export class MembersService {
     this.membersObservable = this.membersCollection.valueChanges({ idField: 'id' });
   }
 
+  /**
+   * Retrieve all members
+   */
   get members(): Observable<Member[]> {
     return this.membersObservable;
   }
 
+  /**
+   * Add a member
+   *
+   * @param member The member instance
+   */
   addMember(member: Member): Promise<DocumentReference> {
     return this.membersCollection.add(member);
   }
 
+  /**
+   * Delete member using the id.
+   *
+   * @param id The id of the member
+   */
   async deleteMember(id: string): Promise<void> {
     const memberDoc = this.membersCollection.doc<Member>(id);
     const documentSnapshot = await memberDoc.ref.get();
@@ -42,16 +55,31 @@ export class MembersService {
     return memberDoc.delete();
   }
 
+  /**
+   * Upload a member photo
+   *
+   * @param file The file instance of the profile photo of the member.
+   */
   uploadPhoto(file: File): AngularFireUploadTask {
     const filePath = 'members/' + new Date().valueOf() + '-' + file.name;
-    // const fileRef = this.fireStorage.ref(filePath);
     return this.fireStorage.upload(filePath, file);
   }
 
+  /**
+   * Delete the profile photo of the member.
+   *
+   * @param fileUrl The url of the photo.
+   */
   deletePhotoByUrl(fileUrl: string): Promise<any> {
     return this.fireStorage.storage.refFromURL(fileUrl).delete();
   }
 
+  /**
+   * Update the member
+   *
+   * @param id The id of the member
+   * @param member The instance of the member
+   */
   updateMember(id: string, member: Member): Promise<void> {
     return this.membersCollection.doc<Member>(id).update(member);
   }
