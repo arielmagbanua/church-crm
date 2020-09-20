c3009e91edb2cddb3632a0d9de277134ee23cb99
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-09-20 21:35:43 +0800

    made the leader optiona.

--- src/app/small-groups/small-group.ts
+++ src/app/small-groups/small-group.ts
@@ -3,5 +3,5 @@ import { DocumentReference } from '@angular/fire/firestore';
 export interface SmallGroup {
   id: string;
   name: string;
-  leader: DocumentReference;
+  leader?: DocumentReference;
 }
